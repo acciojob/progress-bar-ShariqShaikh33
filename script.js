17a012fc8188a2333840f41d17afd449e7ebef6c
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-01-15 19:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const prevbtn = document.getElementById("prev");
+const nextbtn = document.getElementById("next");
+
+console.log(prevbtn);
+
+function prevfunc() {
+	console.log("prev");
+	document.getElementById("circle-1").innerHTML = "done";
+}
+
+prevbtn.addEventListener("click",()=>{prevfunc()});
+
+
+
+console.log("hiii");
